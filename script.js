18d961edccf912259b4255f867d4c355c63ba67b
Andrey <GirsaVit@tut.by>
2021-01-04 22:41:53 +0300

    new_commit

--- script.js
+++ script.js
@@ -1,32 +1,40 @@
 "use strict"
 
-const buttonBack=document.querySelector(".button__back");
-const buttonNext=document.querySelector(".button__next")
-const solutionTitle=document.querySelector(".solution_title");
+const buttonBack = document.querySelector(".button__back");
+const buttonNext = document.querySelector(".button__next")
+const solutionTitle = document.querySelector(".solution_title");
+const slider = document.querySelector(".slider_container");
 
-let solutionTitleValues=["Food Delivery","Women's Clothing Webstore","Furniture Constructor"];
+let solutionTitleValues = ["Food Delivery", "Women's Clothing Webstore", "Furniture Constructor"];
 
-let solutionTitleCurrentValue=0
+let solutionTitleCurrentValue = 0;
+let translateValue = "1100";
 
-buttonNext.addEventListener("click",()=>{
-    if(solutionTitleCurrentValue<2){
-        solutionTitleCurrentValue++
-        solutionTitle.textContent=solutionTitleValues[solutionTitleCurrentValue]
+buttonBack.setAttribute("disabled", "disabled");
+
+buttonNext.addEventListener("click", () => {
+    solutionTitleCurrentValue++;
+    if(buttonBack.hasAttribute("disabled")){
+        buttonBack.removeAttribute("disabled")
     }
-    else{
-        solutionTitleCurrentValue=0;
-        solutionTitle.textContent=solutionTitleValues[solutionTitleCurrentValue]
+    solutionTitle.textContent = solutionTitleValues[solutionTitleCurrentValue % 3]
+    slider.style.transform = `translateX(-${translateValue * solutionTitleCurrentValue}px)`
+    if (solutionTitleCurrentValue === 2) {
+        buttonNext.setAttribute("disabled", "disabled")
     }
 })
 
-buttonBack.addEventListener("click",()=>{
-    if(solutionTitleCurrentValue===0){
-        solutionTitleCurrentValue=2
-        solutionTitle.textContent=solutionTitleValues[solutionTitleCurrentValue]
+buttonBack.addEventListener("click", () => {
+    if(buttonNext.hasAttribute("disabled")){
+        buttonNext.removeAttribute("disabled")
     }
-    else{
-        solutionTitleCurrentValue--;
-        solutionTitle.textContent=solutionTitleValues[solutionTitleCurrentValue]
+    solutionTitleCurrentValue--;
+    solutionTitle.textContent = solutionTitleValues[solutionTitleCurrentValue % 3]
+    if (solutionTitleCurrentValue === 0) {
+        buttonBack.setAttribute("disabled", "disabled")
+        slider.style.transform = `translateX(0)`
+    } else {
+        slider.style.transform=`translateX(-${translateValue * solutionTitleCurrentValue}px)`
     }
 })
 
